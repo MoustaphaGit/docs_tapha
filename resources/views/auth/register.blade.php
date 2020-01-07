b2419e30fc2c0b5fa33f7435c8a4b2d06173f421
user.email <ngomtafa@gmail.com>
2020-01-07 20:44:39 +0000

    query ok sur webpack mix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,17 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label for="status" class="col-md-4 col-form-label text-md-right">{{ __('status') }}</label>
+
+                            <div class="col-md-6">
+                                <select name="status" id="status" class="form-control" >
+                                    <option value="1">Annonceur</option>
+                                    <option value="2">User</option>
+                                </select>
+                                <input id="status" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js(['resources/js/app.js','resources/js/owl.carousel.min.js'], "public/js")
+mix.js(['resources/js/app.js','resources/js/owl.carousel.min.js','resources/js/db.js','resources/js/form-file-upload-data.js','resources/js/init.js',
+    'resources/js/dropdown-bootstrap-extended.js',
+    'resources/js/feather.min.js','resources/js/dataTables-data.js','resources/js/form-wizard-data.js',
+    'resources/js/login-data.js','resources/js/validation-data.js','resources/js/owl-data.js'], "public/js")
    .sass('resources/sass/app.scss', 'public/css')
 
-   /* .styles(['resources/css/style_2.css','resources/css/style.css',
-   'resources/css/simple-line-icons.css','resources/css/linea-icon.css',
-   'resources/css/lightgallery.css'], "public/css/all.css"); */
+   .styles(['resources/css/style_2.css','resources/css/style.css','resources/css/simple-line-icons.css',
+       'resources/css/linea-icon.css','resources/css/ionicons.min.css'], "public/css/docs.css");
